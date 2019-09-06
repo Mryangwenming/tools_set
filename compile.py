201a3b395fd3dcb1b4d7592a758a790707a9d1cc
杨文明 <v-yangwenming@ainnovation.com>
2019-09-06 14:26:48 +0800

    fix bug

--- compile.py
+++ compile.py
@@ -17,6 +17,20 @@ def get_py_file(root_path):
                 yield node_path
 
 
+def exec(file_path, abspath, name):
+    cmd = "python -m nuitka --module --recurse-none --plugin-enable=pylint-warnings {} --output-dir={}".format(file_path, abspath)
+    _, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    if err:
+        print("Error compiling project: {}".format(err))
+    try:
+        shutil.rmtree(os.path.join(abspath, "{}.build".format(name)))
+        os.remove(os.path.join(abspath, "{}.pyi".format(name)))
+        os.remove(file_path)
+    except Exception as e:
+        print("Delete file is error: {}".format(e))
+
+
+
 def add_argument():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--file_path")
@@ -26,46 +40,46 @@ def add_argument():
 
 
 def compile_file(file_path, filter_set):
-    for file in get_py_file(file_path):
-        file_name = os.path.split(file)[-1]
-        abspath = os.path.split(file)[0]
+    if os.path.isfile(file_path):
+        file_name = os.path.split(file_path)[-1]
+        abspath = os.path.split(file_path)[0]
         name = file_name.split(".")[0]
+        exec(file_path, abspath, name)
+        print("Complete compilation!")
 
-        if name in filter_set:
-            pass
-        else:
-            cmd = "python -m nuitka --module --recurse-none --plugin-enable=pylint-warnings {} --output-dir={}".format(file, abspath)
-            _, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            if err:
-                print("Error compiling project: {}".format(err))
-            try:
-                shutil.rmtree(os.path.join(abspath, "{}.build".format(name)))
-                os.remove(os.path.join(abspath, "{}.pyi".format(name)))
-                os.remove(file)
-            except Exception as e:
-                print("Delete file is error: {}".format(e))
+    elif os.path.isdir(file_path):
+        for file in get_py_file(file_path):
+            # name_path = os.path.split(i)[0]
+            # if name_path in {"/home/ywm/Desktop/ManuVision/sirius/app/services", "/home/ywm/Desktop/ManuVision/sirius/app/ops"}:
+            file_name = os.path.split(file)[-1]
+            abspath = os.path.split(file)[0]
+            name = file_name.split(".")[0]
+            if name in filter_set:
+                pass
+            else:
+                exec(file, abspath, name)
+        print("Complete compilation!")
+    else:
+        print("输入的文件路径有误!")
 
-    print("Complete compilation!")
 
 
 def main():
     args = add_argument()
-    try:
-        file_path = args.file_path
-        filter_set = args.filter_file.split(",")
+    file_path = args.file_path
+    filter_set = args.filter_file
+
+    if not file_path:
+        print("-p 参数不存在!")
+    elif not filter_set:
+        compile_file(file_path, [])
+    else:
+        filter_set = filter_set.split(",")
         compile_file(file_path, filter_set)
-    except AttributeError:
-        print("输入的参数不完整")
 
 
 if __name__ == '__main__':
+    # file_path = "/home/ywm/Desktop/ManuVision/sirius"
+    # filter_set = {"uwsgi.py", "uwsgi_entry.py"}
+    # main(file_path, filter_set)
     main()
-
-
-
-
-
-
-
-
-
