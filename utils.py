5b79ce33cec75e0bc4d2d3d74a5c6850369eb23a
yangwenming <1593342097@qq.com>
2020-06-14 13:49:54 +0800

    增加签名函数方法

--- utils.py
+++ utils.py
@@ -9,3 +9,17 @@ def get_nest_key(dict1, res=None):
         else:
             res.append(value)
             yield res
+
+# 签名函数
+def sign(public_data, json_data, app_secret):
+    args = []
+    json_data.update(public_data)
+    for k in sorted(json_data.keys()):
+        v = json_data.get(k)
+        if v is None or v == '' or isinstance(v, list) or isinstance(v, dict):
+            continue
+        args.append('{}={}'.format(k, v))
+    sign_str = '&'.join(args)
+    sign = hmac.new(app_secret.encode(), sign_str.encode(), hashlib.sha1)
+    this_sign = sign.hexdigest()
+    return this_sign.upper()
