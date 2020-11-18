c96d94a3c26bedc61fc1419aa237552255bdac42
杨文明 <44916858+Mryangwenming@users.noreply.github.com>
2020-11-18 16:37:26 +0800

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -47,7 +47,81 @@ class SshClient(object):
     def download_file(self, remote_path, local_path):
         sftp = paramiko.SFTPClient.from_transport(self.transport)
         sftp.get(remote_path, local_path)
+   
 
+class DataBaseConnection:
+    """
+    使用用户名密码连接mysql数据库
+    """
+    def __init__(self, host, username, passwd, db):
+        try:
+            self.conn = pymysql.connect(host=host, user=username, password=passwd, database=db)
+            self.cursor = self.conn.cursor(pymysql.cursors.DictCursor)
+        except Exception:
+            print('连接失败，请检查数据库配置！')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cursor.close()
+        self.conn.close()
+
+    def query_many(self, sql):
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
+
+    def exec(self, sql):
+        self.cursor.execute(sql)
+
+    def exec_many(self, template, args):
+        self.cursor.executemany(template, args)
+
+    def commit(self):
+        self.conn.commit()
+
+
+class SshDataBaseConn:
+    """
+    通过ssh连接docker容器的mysql数据库
+    """
+    def __init__(self, ssh_host, ssh_username, ssh_passwd, db_user, db_passwd, db):
+        try:
+            self.server = SSHTunnelForwarder(ssh_address_or_host=(ssh_host, 22),
+                                        ssh_username=ssh_username,
+                                        ssh_password=ssh_passwd,
+                                        remote_bind_address=('localhost', 3306))
+            self.server.start()
+
+            self.conn = pymysql.connect(host='127.0.0.1',
+                                   port=self.server.local_bind_port,
+                                   user=db_user,
+                                   password=db_passwd,
+                                   database=db)
+            self.cursor = self.conn.cursor(pymysql.cursors.DictCursor)
+        except Exception:
+            print('连接失败，请检查数据库配置！')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cursor.close()
+        self.conn.commit()
+        self.server.close()
+
+    def query_many(self, sql):
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
+
+    def exec(self, sql):
+        self.cursor.execute(sql)
+
+    def exec_many(self, template, args):
+        self.cursor.executemany(template, args)
+
+    def commit(self):
+        self.conn.commit()
 
 if __name__ == '__main__':
     pass
